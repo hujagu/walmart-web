0092327bf19b5779c84dc80b94a3390d166782e4
jgarrido <javier.garrido@latam.com>
2020-10-25 18:45:07 -0300

    add call to service

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -5,11 +5,32 @@ import './searchBar.css';
 
 
 class SearchBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: undefined
+        };
+        this.getData = this.getData.bind(this);
+    }
+
+    getData = async (e) => {
+        e.preventDefault();
+        const search = e.target.elements.search.value;
+        const getData = await fetch(`https://walmart-service.herokuapp.com/products?search=${search}`);
+        //const getData = await fetch(`http://localhost:5000/products?search=${search}`);
+        const data = await getData.json();
+
+        this.setState({
+            products: data
+        });
+    }
+
     render() {
         return <header className="site-header">
             <div className="container">
                 <Logo />
-                <SearchBox />
+                <SearchBox getData={this.getData}/>
             </div>
         </header>
     }
--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -5,9 +5,9 @@ import './searchBox.css';
 class SearchBox extends React.Component {
     render() {
         return <div id="searchBox" className="search-box">
-            <form action="#" id="searchBoxForm" method="GET" className="form-new search-form">
-            <input type="text" name="Ntt" placeholder="¿Qué producto buscas hoy?" id="searchtextinput" autocomplete="off" className="form-control search-input" />
-                <button type="button" id="searchsubmitbutton" className="search-btn">
+            <form action="#" id="searchBoxForm" onSubmit={this.props.getData} method="GET" className="form-new search-form">
+                <input type="text" name="search" placeholder="¿Qué producto buscas hoy?" id="searchtextinput" autocomplete="off" className="form-control search-input" />
+                <button type="submit" id="searchsubmitbutton" className="search-btn">
                     <BsSearch />
                 </button>
             </form>
